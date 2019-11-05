bc3f7ba672078fe484a13cd2bbdeb2b44a6e7785
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-11-05 10:12:58 -0500

    Initialized database. Tables created

---
+++ schema.sql
@@ -0,0 +1,25 @@
+DELETE DATABASE IF EXISTS employeeDB;
+
+CREATE DATABASE employeeDB;
+
+USE employeeDB;
+
+CREATE TABLE department (
+    id INT auto_increment PRIMARY KEY NOT NULL,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT auto_increment PRIMARY KEY NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(9,2) NOT NULL,
+    department_id INT NOT NULL
+);
+
+CREATE TABLE employee (
+    id INT auto_increment PRIMARY KEY NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT NULL
+);
