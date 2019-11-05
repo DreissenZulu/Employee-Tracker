99c420921aa0abca827bf9776f0c094ae07b404c
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-11-05 16:55:10 -0500

    Package dependencies added

--- app.js
+++ app.js
@@ -6,3 +6,11 @@ const port = process.env.port || 3000;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+app.get("*", (req, res) => {
+    res.send(`Successful get to wildcard`);
+})
+
+app.listen(port, () => {
+    console.log(`Now listening to port ${port}.`)
+})
