a85b1fb1da5e2715dd13a8c8b41d7c14aa9c6448
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-11-06 00:17:12 -0500

    Add employee function completed

--- app.js
+++ app.js
@@ -44,6 +44,44 @@ async function showEmployeeSummary() {
     });
 }
 
+async function addEmployee() {
+    let positions = await db.query('SELECT id, title FROM role');
+    let managers = await db.query('SELECT id, CONCAT(first_name, " ", last_name) AS Manager FROM employee');
+    managers.unshift({id: null, Manager: "None"});
+
+    inquirer
+        .prompt([{
+            name: "firstName",
+            type: "input",
+            message: "Enter employee's first name:"
+        },
+        {
+            name: "lastName",
+            type: "input",
+            message: "Enter employee's last name:"
+        },
+        {
+            name: "role",
+            type: "list",
+            message: "Choose employee role:",
+            choices: positions.map(obj => obj.title)
+        },
+        {
+            name: "manager",
+            type: "list",
+            message: "Choose the employee's manager:",
+            choices: managers.map(obj => obj.Manager)
+        }
+        ]).then((answers) => {
+            let positionDetails = positions.find(obj => obj.title === answers.role);
+            let manager = managers.find(obj => obj.Manager === answers.manager);
+            db.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?)",
+            [[answers.firstName, answers.lastName, positionDetails.id, manager.id]]);
+            console.log(`${answers.firstName} was added to the employee database!`);
+            runApp();
+        });
+}
+
 function runApp() {
     inquirer
         .prompt({
@@ -64,6 +102,7 @@ function runApp() {
                     showEmployeeSummary();
                     break;
                 case "Add A New Employee":
+                    addEmployee();
                     break;
                 case "Update Employee Info":
                     break;
@@ -78,3 +117,10 @@ function runApp() {
 }
 
 runApp();
+
+// async function testFunc() {
+//     let employees = await db.query('SELECT * FROM employee');
+//     console.log(employees);
+// }
+
+// testFunc();
