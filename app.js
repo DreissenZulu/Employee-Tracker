a16fa6e0da9885d600b3fda1eea808bc55552f83
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-11-05 21:04:46 -0500

    Test function now properly displays table using SQL methods

--- app.js
+++ app.js
@@ -1,18 +1,49 @@
-const express = require("express");
-const sql = require("mysql");
+const mysql = require("mysql");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
 
-const app = express();
-const port = process.env.port || 3000;
+class Database {
+    constructor(config) {
+        this.connection = mysql.createConnection(config);
+    }
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
+    query(sql, args) {
+        return new Promise((resolve, reject) => {
+            this.connection.query(sql, args, (err, rows) => {
+                if (err)
+                    return reject(err);
+                resolve(rows);
+            });
+        });
+    }
 
-app.get("*", (req, res) => {
-    res.send(`Successful get to wildcard`);
-})
+    close() {
+        return new Promise((resolve, reject) => {
+            this.connection.end(err => {
+                if (err)
+                    return reject(err);
+                resolve();
+            });
+        });
+    }
+}
 
-app.listen(port, () => {
-    console.log(`Now listening to port ${port}.`)
-})
+const db = new Database({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "bAVqss3GpRKQ",
+    database: "employeeDB"
+});
+
+async function testFunc() {
+    await db.query('SELECT e.id, e.first_name AS First_Name, e.last_name AS Last_Name, title AS Title, salary AS Salary, name AS Department, CONCAT(m.first_name, " ", m.last_name) AS Manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role r ON e.role_id = r.id INNER JOIN department d ON r.department_id = d.id', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+    });
+}
+
+
+testFunc();
+
+//
