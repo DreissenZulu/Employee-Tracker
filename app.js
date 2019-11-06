5091072f2fe9ed82de2611a3427c694dd60aad67
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-11-06 10:43:23 -0500

    Modify employee manager function is operational

--- app.js
+++ app.js
@@ -50,70 +50,97 @@ async function addEmployee() {
     let managers = await db.query('SELECT id, CONCAT(first_name, " ", last_name) AS Manager FROM employee');
     managers.unshift({ id: null, Manager: "None" });
 
-    inquirer
-        .prompt([
-            {
-                name: "firstName",
-                type: "input",
-                message: "Enter employee's first name:"
-            },
-            {
-                name: "lastName",
-                type: "input",
-                message: "Enter employee's last name:"
-            },
-            {
-                name: "role",
-                type: "list",
-                message: "Choose employee role:",
-                choices: positions.map(obj => obj.title)
-            },
-            {
-                name: "manager",
-                type: "list",
-                message: "Choose the employee's manager:",
-                choices: managers.map(obj => obj.Manager)
-            }
-        ]).then((answers) => {
-            let positionDetails = positions.find(obj => obj.title === answers.role);
-            let manager = managers.find(obj => obj.Manager === answers.manager);
-            db.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?)", [[answers.firstName, answers.lastName, positionDetails.id, manager.id]]);
-            console.log(`${answers.firstName} was added to the employee database!`);
-            runApp();
-        });
+    inquirer.prompt([
+        {
+            name: "firstName",
+            type: "input",
+            message: "Enter employee's first name:"
+        },
+        {
+            name: "lastName",
+            type: "input",
+            message: "Enter employee's last name:"
+        },
+        {
+            name: "role",
+            type: "list",
+            message: "Choose employee role:",
+            choices: positions.map(obj => obj.title)
+        },
+        {
+            name: "manager",
+            type: "list",
+            message: "Choose the employee's manager:",
+            choices: managers.map(obj => obj.Manager)
+        }
+    ]).then(answers => {
+        let positionDetails = positions.find(obj => obj.title === answers.role);
+        let manager = managers.find(obj => obj.Manager === answers.manager);
+        db.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?)", [[answers.firstName, answers.lastName, positionDetails.id, manager.id]]);
+        console.log(`${answers.firstName} was added to the employee database!`);
+        runApp();
+    });
 };
 
 async function removeEmployee() {
     let employees = await db.query('SELECT id, CONCAT(first_name, " ", last_name) AS name FROM employee');
     employees.push({ id: null, name: "Cancel" });
 
-    inquirer
-        .prompt([
-            {
-                name: "employeeName",
-                type: "list",
-                message: "Remove which employee?",
-                choices: employees.map(obj => obj.name)
-            }
-        ]).then((response) => {
-            if (response.employeeName != "Cancel") {
-                let unluckyEmployee = employees.find(obj => obj.name === response.employeeName);
-                db.query("DELETE FROM employee WHERE id=?", unluckyEmployee.id);
-                console.log(`${response.employeeName} was let go...`);
-            }
-            runApp();
-        })
+    inquirer.prompt([
+        {
+            name: "employeeName",
+            type: "list",
+            message: "Remove which employee?",
+            choices: employees.map(obj => obj.name)
+        }
+    ]).then(response => {
+        if (response.employeeName != "Cancel") {
+            let unluckyEmployee = employees.find(obj => obj.name === response.employeeName);
+            db.query("DELETE FROM employee WHERE id=?", unluckyEmployee.id);
+            console.log(`${response.employeeName} was let go...`);
+        }
+        runApp();
+    })
 };
 
 async function updateManager() {
-    inquirer
-    .prompt([
+    let employees = await db.query('SELECT id, CONCAT(first_name, " ", last_name) AS name FROM employee');
+    employees.push({ id: null, name: "Cancel" });
+
+    inquirer.prompt([
         {
-            name: "targetEmployee",
+            name: "empName",
             type: "list",
-            message: ""
+            message: "For which employee?",
+            choices: employees.map(obj => obj.name)
         }
-    ])
+    ]).then(employeeInfo => {
+        if (employeeInfo.empName == "Cancel") {
+            runApp();
+            return;
+        }
+        let managers = employees.filter(currEmployee => currEmployee.name != employeeInfo.empName);
+        for (i in managers) {
+            if (managers[i].name === "Cancel") {
+                managers[i].name = "None";
+            }
+        };
+
+        inquirer.prompt([
+            {
+                name: "mgName",
+                type: "list",
+                message: "Change their manager to:",
+                choices: managers.map(obj => obj.name)
+            }
+        ]).then(managerInfo => {
+            let empID = employees.find(obj => obj.name === employeeInfo.empName).id
+            let mgID = managers.find(obj => obj.name === managerInfo.mgName).id
+            db.query("UPDATE employee SET manager_id=? WHERE id=?", [mgID, empID]);
+            console.log(`${employeeInfo.empName} now reports to ${managerInfo.mgName}`);
+            runApp();
+        })
+    })
 };
 
 async function addRole() {
@@ -125,41 +152,40 @@ async function addDepartment() {
 };
 
 function runApp() {
-    inquirer
-        .prompt({
-            name: "mainmenu",
-            type: "list",
-            message: "What would you like to do?",
-            choices: [
-                "View All Employees",
-                "Add A New Employee",
-                "Update Employee Manager",
-                "Remove An Employee",
-                "Add A New Role",
-                "Add A New Department"
-            ]
-        }).then((responses) => {
-            switch (responses.mainmenu) {
-                case "View All Employees":
-                    showEmployeeSummary();
-                    break;
-                case "Add A New Employee":
-                    addEmployee();
-                    break;
-                case "Update Employee Manager":
-                    updateManager();
-                    break;
-                case "Remove An Employee":
-                    removeEmployee();
-                    break;
-                case "Add A New Role":
-                    addRole();
-                    break;
-                case "Add A New Department":
-                    addDepartment();
-                    break;
-            }
-        });
+    inquirer.prompt({
+        name: "mainmenu",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "View All Employees",
+            "Add A New Employee",
+            "Update Employee Manager",
+            "Remove An Employee",
+            "Add A New Role",
+            "Add A New Department"
+        ]
+    }).then((responses) => {
+        switch (responses.mainmenu) {
+            case "View All Employees":
+                showEmployeeSummary();
+                break;
+            case "Add A New Employee":
+                addEmployee();
+                break;
+            case "Update Employee Manager":
+                updateManager();
+                break;
+            case "Remove An Employee":
+                removeEmployee();
+                break;
+            case "Add A New Role":
+                addRole();
+                break;
+            case "Add A New Department":
+                addDepartment();
+                break;
+        }
+    });
 }
 
 runApp();
